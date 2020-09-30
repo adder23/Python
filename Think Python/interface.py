54aa6638a25b5b4ea96df6b266ac6f121ff5bf87
adder23 <rafak0@protonmail.com>
2020-10-01 01:31:26 +0300

    oh man I was half sleeping most of the time doing this refactoring ... I need sleep !

--- Think Python/interface.py
+++ Think Python/interface.py
@@ -1,42 +1,39 @@
 import turtle
 import math
 
+
 def square(t,length):
-    t = turtle.Turtle()
-    print(t)
-    
     for i in range(4):
         t.fd(length)
         t.lt(90)
     
-    turtle.mainloop()
-
-#square("bob",200)
-
-def polygon(t,length,n,angle):
-    t = turtle.Turtle()
-    print(t)
-    
+def polyline(t,length,n,angle):
     for i in range(n):
         t.fd(length)
-        t.lt(angle/n)
-    turtle.mainloop()
+        t.lt(angle)
 
-#polygon("bob",150,6)
+def polygon(t, length, n):
+    angle = 360/n
+    polyline(t, length, n, angle)
 
 def circle(t,r):
-    circ = math.pi * r * 2
-    n = int(r/2)
-    length = circ/n
-    polygon(t,length,n)
-
-#circle("bob",50)
+    arc(t,r,360)
 
 def arc(t,r,angle):
-    circ = math.pi * r * 2
+    arc_length = math.pi * r * 2 * angle / 360
     n = int(r/2)
-    length = circ/n
-    polygon(t,length,n,angle)
+    step_length = arc_length / n
+    step_angle = float(angle) / n
+    polyline(t,step_length,n,step_angle)
 
-arc("bob",50,180)
+bob = turtle.Turtle()
+
+#square(bob, 200)
+#polygon(bob,150,5)
+#circle(bob,50)
+#arc(bob,50,360)
+
+print(bob)
+turtle.mainloop()
+
 #okay this is what I wrote after reading the examples now that I've read ahead I will rewrite my code in my next session, Damn the incoming tasks do require some amount of math, don't they ?
